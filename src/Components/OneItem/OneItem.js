3cfcc9f24c63e1a8b79c1cad48c848b0978d4ef2
Niamul.Hasan45 <niamul.rony@gmail.com>
2022-05-02 20:26:35 +0600

    adding-deleting

--- src/Components/OneItem/OneItem.js
+++ src/Components/OneItem/OneItem.js
@@ -6,7 +6,7 @@ import './OneItem.css';
 
 const OneItem = () => {
 
-    const  [remaining, setRemainig]= useState(0);
+    const [remaining, setRemainig] = useState(0);
 
     const itemRef = useRef();
 
@@ -19,16 +19,32 @@ const OneItem = () => {
         fetch(url)
             .then(res => res.json())
             .then(data => setItem(data))
-    }, [])
+    }, [remaining])
 
-    const {img, name, supplier, description, price, quantity } = item;
+    const { img, name, supplier, description, price, quantity } = item;
 
-    const handleProduct=(quantity)=>{
-        if(quantity>0){
-            let remainingPro = quantity-1;
-            setRemainig(remainingPro); 
+    const handleProduct = (number) => {
+        if (number > 0) {
+            let quantity = number - 1;
+
+            // setRemainig(number);
+            const data = { quantity };
+
+            // setRemainig(quantity);
+            const url = `http://localhost:5000/inventory/${id}`;
+            fetch(url, {
+                method: 'PUT',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            })
+                .then(res => res.json())
+                .then(result => {
+                    setRemainig(result);
+                })
         }
-        else{
+        else {
             alert('No product is available');
         }
 
@@ -36,26 +52,28 @@ const OneItem = () => {
 
 
 
-    const handleForm = event =>{
-        event.preventDefault();
-        const number = itemRef.current.value;
-        setRemainig(number);
-        console.log(remaining);
+    const handleForm = number => {
+        // event.preventDefault();
+
+        // console.log(event);
+        const quantity = parseInt(itemRef.current.value)+number;
+        // setRemainig(number);
+        const data = { quantity };
 
 
 
-        // const url = `http://localhost:5000/inventory/${id}`;
-        // fetch(url, {
-        //     method: 'PUT',
-        //     headers: {
-        //         'content-type': 'application/json'
-        //     },
-        //     body: JSON.stringify(remaining)
-        // })
-        // .then(res=> res.json())
-        // .then(result =>{
-        //     console.log(result);
-        // } )
+        const url = `http://localhost:5000/inventory/${id}`;
+        fetch(url, {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+            .then(res => res.json())
+            .then(result => {
+                setRemainig(result);
+            })
     }
 
 
@@ -71,12 +89,12 @@ const OneItem = () => {
                     <p>Supplier Name: {supplier}</p>
                     <p className='mb-5'><small>{description}</small></p>
                     <h4>Available Quantity: {quantity}</h4>
-                    <button onClick={()=>handleProduct(quantity)} className='btn btn-primary px-4 py-2 mt-3'>Deliver this item</button>
-                    <form onSubmit={handleForm} className='m-5'>
+                    <button onClick={() => handleProduct(quantity)} className='btn btn-primary px-4 py-2 mt-3'>Deliver this item</button>
+                    <form  className='m-5'>
                         <label className='m-3 text-primary'><b>Re-stock this product:</b> </label>
                         <input type="number" ref={itemRef} required placeholder='Amount of product' />
                         <button className='
-                        btn btn-warning m-3' type="submit">Submit</button>
+                        btn btn-warning m-3' onClick={()=>handleForm(quantity)}>Submit</button>
                     </form>
                 </div>
             </div>
