e0f94d28cc38ca1481ae320d1233cb7b05ad614f
Niamul.Hasan45 <niamul.rony@gmail.com>
2022-05-07 21:04:55 +0600

    error resolved

--- src/Homepage/Inventory/Inventory.js
+++ src/Homepage/Inventory/Inventory.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link} from 'react-router-dom';
 import PlanTimeline from '../../Components/PlanTimeline/PlanTimeline';
 import Item from '../Items/Item';
 import './Inventory.css'
